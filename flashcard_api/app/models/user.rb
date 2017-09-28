4ee2dfa5a9f51aef8975607d62d9f586de474567
lizdenhup <lizdenhup@gmail.com>
2017-09-28 15:44:39 -0500

    switched to built-in rails email validator method

--- flashcard_api/app/models/user.rb
+++ flashcard_api/app/models/user.rb
@@ -1,21 +1,6 @@
-class EmailValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-        unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-        # / start of regular exp
-        # \A start of string
-        # (...) capture everything enclosed that is
-        # ^any single character except @, whitespace
-        # @ is between capture groups
-        # second capture group is alphanumeric
-        # \z is end of string
-        # \. is the dot
-        # [a-z]{2,}\z is 2 or more of  anything in the a-z range
-        record.errors[attribute] << (options[:message] || "is not an email")
-      end
-    end
-end 
-
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true, email: true 
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :email, presence: true
+    
 end
